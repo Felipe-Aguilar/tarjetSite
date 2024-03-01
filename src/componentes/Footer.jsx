5593939be89f52b1e2a584d83f135875e9be07fa
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2024-03-01 10:30:57 -0600

    Cambios de viernes 01 de marzo

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -22,9 +22,9 @@ const Footer = () => {
             </div>
 
             <div className='nav'>
-                <a href="">Política de Envío</a>
+                {/* <a href="">Política de Envío</a>
                 <a href="">Términos y Condiciones</a>
-                <a href="">Política de Devoluciones</a>
+                <a href="">Política de Devoluciones</a> */}
                 <a onClick={()=>navigate('/aviso-privacidad')}>Aviso de Privacidad</a>
                 {/* <a href="">Facturación</a> */}
                 <a href="">Contacto</a>
